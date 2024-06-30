015536dcfc7d05fd007d6d4ef6d9d7fb128c1231
Daniel Gahler <darkshoxx@yahoo.de>
2024-06-30 19:36:18 +0100

    outline of functionality

--- SYNKABOT/main.py
+++ SYNKABOT/main.py
@@ -1,9 +1,9 @@
 # Base imports
-import html
+import asyncio
 import os
 import re
 from hashlib import md5
-from typing import Tuple, Union
+from typing import List, Tuple, Union
 
 # Installed Imports
 import aiohttp
@@ -12,8 +12,7 @@ from discord.abc import GuildChannel
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
-# Local Imports
-from SYNKABOT.blog_list import BLOGS, NON_WP_BLOGS
+
 
 load_dotenv()
 
@@ -49,69 +48,6 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-async def fetch_latest_wp_blog_post(
-    session: aiohttp.ClientSession, blog_dict: dict
-) -> Tuple[int, str]:
-    """ Gets the most recent blog post from a Wordpress blog.
-    Parameters:
-    session (aiohttp.ClientSession): client session to make GET request
-    blog_dict (dict): dict containing info needed to fetch blog and make string
-
-    Returns: blog_id (int), print_string
-    """
-    blog_api = blog_dict["url"]
-    # asynch context manager (!)
-    async with session.get(
-        blog_api + "/posts", params={"per_page": 1, "orderby": "date"}
-    ) as response:
-        response.raise_for_status()
-        try:
-            latest_post = await response.json()
-            post_id = latest_post[0]["id"]
-            title = html.unescape(latest_post[0]["title"]["rendered"])
-            # TODO different encoding for posting time, date is fine,
-            # time not really helpful.
-            posting_time = latest_post[0]["date"]
-            post_url = latest_post[0]["link"]
-            author = blog_dict["author"]
-            post_print_string = (
-                "HEY, LISTEN! \nNew Blog post from "
-                + f"{author}! Released on {posting_time}!\nTitle: {title} "
-                + f"\nLink:{post_url}"
-            )
-            return post_id, post_print_string
-        except (IndexError, KeyError) as e:
-            print(f"Error parsing JSON: {e}")
-            return None, None
-
-
-async def fetch_latest_non_wp_blog_post(
-    session: aiohttp.ClientSession, blog_dict: dict
-) -> Tuple[str, str]:
-    """ Gets the most recent blog post from a Non-Wordpress blog.
-    Parameters:
-    session (aiohttp.ClientSession): client session to make GET request
-    blog_dict (dict): dict containing info needed to fetch blog and make string
-
-    Returns: blog_slug (str), print_string (str)
-    """
-    update_url = blog_dict["url"] + blog_dict["update_route"]
-    async with session.get(update_url) as response:
-        response.raise_for_status()
-
-        script_content = re.search(r" /blog/slug/[^;]+", await response.text())
-        if script_content:
-            lastest_post_route = script_content.group().strip()
-            author = blog_dict["author"]
-            post_print_string = (
-                "HEY, LISTEN! \nNew Blog post from "
-                + f"{author}! \n Link:{blog_dict['url'] + lastest_post_route}"
-            )
-            return lastest_post_route, post_print_string
-        else:
-            print("Failed to find blog post at URL")
-            return None, None
-
 
 async def send_latest_blog_post(
     channel: GuildChannel,
@@ -131,10 +67,33 @@ async def send_latest_blog_post(
     else:
         raise Exception(f"Channel {channel} not found! GO FIX!")
 
+async def get_new_messages_from_channel(channel: GuildChannel)->List[Tuple[int, int, str]]:
+    # returns list of triplets of userID, messageID and Message string
+    pass
+
+async def regex_check(message:str)-> bool:
+    pass
+
+async def send_warning(user_id:int):
+    pass
+
+async def punishment(user_id:int, msg_id:int):
+    pass
+
+async def consequences(user_id:int, msg_id: int,  message:str):
+    # wait for correctional edits (1 minute)
+    await asyncio.sleep(60)
+    if not regex_check(message):
+        # 4. Send Warning in DM
+        await send_warning(user_id)
+        # wait for correctional edits after message (1 minute)
+        await asyncio.sleep(60)
+        # 5. no correction: remove message, add strike to user
+        await punishment(user_id, msg_id)
 
 # Event decorator for discord bot to loop
 @tasks.loop(seconds=LOOP_SPEED)
-async def blog_post_task(
+async def check_newest_messages(
     session: aiohttp.ClientSession,
     channel: GuildChannel
 ) -> None:
@@ -145,26 +104,18 @@ async def blog_post_task(
     """
     # TODO check session is alive at beginning of each loop
 
-    # Iterate through Wordpress blogs
-    for blog_dict in BLOGS:
-        post_id, blog_info_to_be_printed = await fetch_latest_wp_blog_post(
-            session, blog_dict
-        )
-        if post_id not in blog_dict["buffer"]:
-            # if it's a new post, add it to the buffer
-            blog_dict["buffer"].add(post_id)
-            await send_latest_blog_post(channel, blog_info_to_be_printed)
 
-    # Iterate through other blogs
-    for blog_dict in NON_WP_BLOGS:
-        (
-            id_surrogate_route,
-            blog_info_to_be_printed,
-        ) = await fetch_latest_non_wp_blog_post(session, blog_dict)
-        if id_surrogate_route not in blog_dict["buffer"]:
-            # if it's a new post, add it to the buffer
-            blog_dict["buffer"].add(id_surrogate_route)
-            await send_latest_blog_post(channel, blog_info_to_be_printed)
+    # 1. Check for new messages
+    new_msg_list = await get_new_messages_from_channel(channel)
+
+    # 2. Iterate through messages:
+    for user_id, msg_id, message in new_msg_list:
+
+        # 3. run regex on message
+        msg_valid = await regex_check(message)
+
+        if not msg_valid:
+            await consequences(user_id, msg_id, message )
 
 
 # Event decorator for discord bots
@@ -182,22 +133,119 @@ async def on_ready():
     session = aiohttp.ClientSession()
     # Get Discord Channel object
     channel = bot.get_channel(CHANNEL_ID)
-    for blog_dict in BLOGS:
-        blog_dict["buffer"] = set()
-        post_id, _ = await fetch_latest_wp_blog_post(session, blog_dict)
-        blog_dict["buffer"].add(post_id)
-    for blog_dict in NON_WP_BLOGS:
-        blog_dict["buffer"] = set()
-        id_surrogate_route, _ = await fetch_latest_non_wp_blog_post(
-            session,
-            blog_dict
-        )
-        blog_dict["buffer"].add(id_surrogate_route)
 
-    blog_post_task.start(session, channel)
+    check_newest_messages.start(session, channel)
 
 
 # so the file doesn't run when imported
 if __name__ == "__main__":
     # But when the file is run, run the bot with its token.
     bot.run(BOT_TOKEN)
+
+
+
+### GRAVEYARD
+
+
+# async def fetch_latest_wp_blog_post(
+#     session: aiohttp.ClientSession, blog_dict: dict
+# ) -> Tuple[int, str]:
+#     """ Gets the most recent blog post from a Wordpress blog.
+#     Parameters:
+#     session (aiohttp.ClientSession): client session to make GET request
+#     blog_dict (dict): dict containing info needed to fetch blog and make string
+
+#     Returns: blog_id (int), print_string
+#     """
+#     blog_api = blog_dict["url"]
+#     # asynch context manager (!)
+#     async with session.get(
+#         blog_api + "/posts", params={"per_page": 1, "orderby": "date"}
+#     ) as response:
+#         response.raise_for_status()
+#         try:
+#             latest_post = await response.json()
+#             post_id = latest_post[0]["id"]
+#             title = html.unescape(latest_post[0]["title"]["rendered"])
+#             # TODO different encoding for posting time, date is fine,
+#             # time not really helpful.
+#             posting_time = latest_post[0]["date"]
+#             post_url = latest_post[0]["link"]
+#             author = blog_dict["author"]
+#             post_print_string = (
+#                 "HEY, LISTEN! \nNew Blog post from "
+#                 + f"{author}! Released on {posting_time}!\nTitle: {title} "
+#                 + f"\nLink:{post_url}"
+#             )
+#             return post_id, post_print_string
+#         except (IndexError, KeyError) as e:
+#             print(f"Error parsing JSON: {e}")
+#             return None, None
+
+
+# async def fetch_latest_non_wp_blog_post(
+#     session: aiohttp.ClientSession, blog_dict: dict
+# ) -> Tuple[str, str]:
+#     """ Gets the most recent blog post from a Non-Wordpress blog.
+#     Parameters:
+#     session (aiohttp.ClientSession): client session to make GET request
+#     blog_dict (dict): dict containing info needed to fetch blog and make string
+
+#     Returns: blog_slug (str), print_string (str)
+#     """
+#     update_url = blog_dict["url"] + blog_dict["update_route"]
+#     async with session.get(update_url) as response:
+#         response.raise_for_status()
+
+#         script_content = re.search(r" /blog/slug/[^;]+", await response.text())
+#         if script_content:
+#             lastest_post_route = script_content.group().strip()
+#             author = blog_dict["author"]
+#             post_print_string = (
+#                 "HEY, LISTEN! \nNew Blog post from "
+#                 + f"{author}! \n Link:{blog_dict['url'] + lastest_post_route}"
+#             )
+#             return lastest_post_route, post_print_string
+#         else:
+#             print("Failed to find blog post at URL")
+#             return None, None
+
+
+
+# from: blog_post_task
+
+
+    # # Iterate through Wordpress blogs
+    # for blog_dict in BLOGS:
+    #     post_id, blog_info_to_be_printed = await fetch_latest_wp_blog_post(
+    #         session, blog_dict
+    #     )
+    #     if post_id not in blog_dict["buffer"]:
+    #         # if it's a new post, add it to the buffer
+    #         blog_dict["buffer"].add(post_id)
+    #         await send_latest_blog_post(channel, blog_info_to_be_printed)
+
+    # # Iterate through other blogs
+    # for blog_dict in NON_WP_BLOGS:
+    #     (
+    #         id_surrogate_route,
+    #         blog_info_to_be_printed,
+    #     ) = await fetch_latest_non_wp_blog_post(session, blog_dict)
+    #     if id_surrogate_route not in blog_dict["buffer"]:
+    #         # if it's a new post, add it to the buffer
+    #         blog_dict["buffer"].add(id_surrogate_route)
+    #         await send_latest_blog_post(channel, blog_info_to_be_printed)
+
+#from on_ready:
+
+    # for blog_dict in BLOGS:
+    #     blog_dict["buffer"] = set()
+    #     post_id, _ = await fetch_latest_wp_blog_post(session, blog_dict)
+    #     blog_dict["buffer"].add(post_id)
+    # for blog_dict in NON_WP_BLOGS:
+    #     blog_dict["buffer"] = set()
+    #     id_surrogate_route, _ = await fetch_latest_non_wp_blog_post(
+    #         session,
+    #         blog_dict
+    #     )
+    #     blog_dict["buffer"].add(id_surrogate_route)
