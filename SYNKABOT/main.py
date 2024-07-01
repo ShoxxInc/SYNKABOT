dc20fbe472710794dcf4354a49a52bb7669cc9d1
Daniel Gahler <darkshoxx@yahoo.de>
2024-07-01 13:56:55 +0100

    somewhat working prototype

--- SYNKABOT/main.py
+++ SYNKABOT/main.py
@@ -8,7 +8,7 @@ from typing import List, Tuple, Union
 # Installed Imports
 import aiohttp
 import discord
-from discord import TextChannel
+from discord import Attachment, DMChannel, Message, TextChannel, User
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
@@ -19,6 +19,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID_1"))
 
+SEEN_MESSAGES = []
 
 LOOP_SPEED = 5
 
@@ -68,15 +69,15 @@ async def send_latest_blog_post(
     else:
         raise Exception(f"Channel {channel} not found! GO FIX!")
 
-async def get_new_messages_from_channel(channel: TextChannel)->List[Tuple[int, int, str]]:
+async def get_new_messages_from_channel(channel: TextChannel, num_of_messages:int)->List[Message]:
     # returns list of triplets of userID, messageID and Message string
-    message = await channel.fetch_message(channel.last_message_id)
-    msg_id = message.id
-    user_id = message.author.id
-    message_content = message.content
-    return user_id, msg_id, message_content
+    global SEEN_MESSAGES
+    message_list = [message async for message in channel.history(limit=num_of_messages) if message.id not in SEEN_MESSAGES]
+    new_messages = [message.id for message in message_list]
+    SEEN_MESSAGES = SEEN_MESSAGES + new_messages
+    return message_list
 
-async def regex_check(message:str)-> bool:
+def regex_check(message:str)-> bool:
     # regex pattern, currently checks for two double pipes.
     regex_pattern = r"(\|\|)*(\|\|)"
     # find matches
@@ -84,25 +85,40 @@ async def regex_check(message:str)-> bool:
     # cast to bool
     return bool(match)
 
-async def send_warning(user_id:int):
+def image_check(attachments:List[Attachment])->bool:
+    return_value = True
+    for attachment in attachments:
+        # can abort if any check was False
+        if return_value and len(attachment.filename)>8:
+            if attachment.filename[:8] !="SPOILER_":
+                return_value = False
+    return return_value
+
+async def send_warning(dm_channel:DMChannel, message_content:str):
     # direct message
-    pass
+    channel = "Dark Room"
+    await dm_channel.send(f"Warning! Your message in the spoiler channel {channel} with the content \"{message_content}\" was not properly put in spoilers. Make sure every image is hidden and every piece of text is between a pair of double pipes, e.g. \"\|\|spoiler\|\|\". You have 1 minute to edit the post.")
 
-async def punishment(user_id:int, msg_id:int):
+async def punishment(dm_channel:DMChannel, message:Message):
     # direct message + message removed
-    pass
+    await message.delete()
+    await dm_channel.send("Consider this a warning! Message Deleted!")
 
-async def consequences(user_id:int, msg_id: int,  message:str):
+
+async def consequences(user_id:int, message:Message, channel:TextChannel):
     # wait for correctional edits (1 minute)
-    await asyncio.sleep(60)
-    if not regex_check(message):
+    await asyncio.sleep(5)
+    if not await spoiler_check(message, channel):
         # 4. Send Warning in DM
-        await send_warning(user_id)
+        user = bot.get_user(user_id)
+        dm_channel = await bot.create_dm(user)
+        await send_warning(dm_channel, message.content)
         # wait for correctional edits after message (1 minute)
-        await asyncio.sleep(60)
+        await asyncio.sleep(5)
         # 5. no correction: remove message, add strike to user
-        if not regex_check(message):
-            await punishment(user_id, msg_id)
+        print(message.content)
+        if not await spoiler_check(message, channel):
+            await punishment(dm_channel, message)
 
 # Event decorator for discord bot to loop
 @tasks.loop(seconds=LOOP_SPEED)
@@ -119,18 +135,35 @@ async def check_newest_messages(
 
 
     # 1. Check for new messages
-    new_msg_list = await get_new_messages_from_channel(channel)
+    new_msg_list = await get_new_messages_from_channel(channel, num_of_messages=20)
 
-    print(new_msg_list)
-    if False:
-        # 2. Iterate through messages:
-        for user_id, msg_id, message in new_msg_list:
+    print([msg.content for msg in new_msg_list])
 
-            # 3. run regex on message
-            msg_valid = await regex_check(message)
+    # 2. Iterate through messages: TODO: these should be handelled at the same time, but currently aren't
+    for message in new_msg_list:
 
-            if not msg_valid:
-                await consequences(user_id, msg_id, message )
+        await iterate_through_messages(channel, message)
+    print("END OF LOOP")
+
+async def iterate_through_messages(channel, message):
+    msg_id = message.id
+    user_id = message.author.id
+    message_content = message.content
+        
+        # 3. run regex on message
+    allowed = await spoiler_check(message, channel)
+    print(allowed, message_content, message.attachments)
+
+    if not allowed:
+        await consequences(user_id, message, channel)
+
+async def spoiler_check(message:Message, channel:TextChannel):
+    # update message contents
+    message = await channel.fetch_message(message.id)
+
+    msg_valid = regex_check(message.content)
+    attachments_valid = image_check(message.attachments)
+    return msg_valid and attachments_valid
 
 
 # Event decorator for discord bots
